5eb32a8d2026c7d31efaf8ad9a3afb033462a782
secret <secretisdead@gmail.com>
2018-09-06 08:56:46 -0700

    restore regex converter and ensure it's added

--- shortener.py
+++ shortener.py
@@ -2,10 +2,17 @@ import json
 
 from flask import Blueprint, abort, g, redirect
 
+from regex_converter import RegexConverter
+
 shortener = Blueprint(
 	'shortener',
 	__name__,
 )
+
+def add_regex_converter(state):
+	state.app.url_map.converters['regex'] = RegexConverter
+
+shortener.record_once(add_regex_converter)
 
 @shortener.route('/' + "<regex('([a-zA-Z0-9_\-\/]+)'):short>")
 def check(short):
