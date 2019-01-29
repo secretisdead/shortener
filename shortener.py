abd2f3dff4c8829628df69b360ae73f2d753a7a0
secret <secretisdead@gmail.com>
2019-01-28 17:17:25 -0800

    remove regex converter

--- shortener.py
+++ shortener.py
@@ -2,19 +2,14 @@ import json
 
 from flask import Blueprint, abort, g, redirect
 
-from regex_converter import RegexConverter
-
 shortener = Blueprint(
 	'shortener',
 	__name__,
 )
 
-def add_regex_converter(state):
-	state.app.url_map.converters['regex'] = RegexConverter
-
 shortener.record_once(add_regex_converter)
 
-@shortener.route('/' + "<regex('([a-zA-Z0-9_\-\/]+)'):short>")
+@shortener.route('/<short>')
 def check(short):
 	if not hasattr(g, 'shortener_config') or short not in g.shortener_config:
 		abort(404)
